28d38d0c27433db57f2df4397c003d731f8ec1ea
roaa-alrajabi <roaa.roaaalrajabi@gmail.com>
2019-11-19 16:28:42 +0200

    solution exsercis

---
+++ day1.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+#Q1
+#print ("world hello\t\n"*3)
+
+#Q2
+#print("orangeAcademy\n"*20)
+
+#Q3
+#x=float(1)
+#print(x)
+#print("\n")
+#y=float(2.8)
+#print(y)
+#print("\n")
+#z=float("2")
+#print(z)
+#print("\n")
+#w=float("3.7")
+#print(w)
+#print("\n")
+#print(x,y,z,w,sep='10')
+
+#Q4
+#num1=int(input("input your num"))
+#num2=int(input("input your num"))
+#print(num1*num2)
+
+#Q5
+#num1=int(input("input your num"))
+#num2=int(input("input your num"))
+#num3=int(input("input your num"))
+#num4=int(input("input your num"))
+#num5=int(input("input your num"))
+#print((num1+num2+num3+num4+num5)/5)
+
+#Q6
+#x=1
+#y=2.8
+#z=1j
+#o="orange"
+#A=True
+#print((type(x)))
+#print((type(y)))
+#print((type(z)))
+#print((type(o)))
+#print((type(A)))
+
+#Q7
+#x,y='orange',1
+#X1,X2=100,-10
+#print(x)
+#print(y)
+#print(X1)
+#print(X2)
+
+#Q8
+#start varible with number 5X
+#x=10
+#print(x)
+# has special character X_q$
+#X_q="Orange"
+#print(X_q)
+#has space between varible
+#AB=14 
+#print(AB)
+#the first letter in print id capital 
+#print('10'*10)
+
+#Q9 and Q10 is repeted 
+
+#Q11
+#print('hello, world!')
+#print("Cheers,Mate")
+#""" testing the code"""
+ #Q12
+#name=input("insert your name:")
+#print(name)
+#age=int(input("insert your age:"))
+#print((100-age))
+ 
+ 
+#Q13
+#Base=int(input("insert base:"))
+#print(Base)
+#height=int(input("insert height"))
+#print(height)
+#print(0.5*(Base*height))
+
+#Q14
+#x=11
+#y=3 
+#print(x+y)
+#print(x-y)
+#print(x*y)
+#print(x/y)
+#print(x//y)
+#print(x%y)
+#print(abs(x*-1))
+#print(x)
+#print(float(x))
+#print(divmod(x,y))
+#print(pow(x,y))
+#print(x**y)
+#print(x>y)
+#print(x==y)
+#print(x!=y)
+
+
+
+
+
+
+
+
+
+
+
+
+
