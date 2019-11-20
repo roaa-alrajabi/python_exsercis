7e5a8d0b0f0bb806dd96827f09f1e6aaf0f81e8d
roaa-alrajabi <roaa.roaaalrajabi@gmail.com>
2019-11-20 17:04:20 +0200

    solve exercise

---
+++ day2.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 09:21:54 2019
+
+@author: orange
+"""
+
+#a,b=1,10
+#if a>b:
+  #  print("a>b")
+    
+#elif a<b: 
+  #  print("a<b")
+#else:
+  #  print("a=b")
+    
+    
+#a,b=1,10
+#print(a)
+#print(b)
+#max= a if(a>b) else b
+#print(max)
+
+#if 'a'in ['b','c','a']:
+   # print("a is in list ")
+#else:
+ #   print("a is not in list ")
+    
+#a=10
+#b=5
+#if a>b:print("a is greate than b")
+
+
+#a=2
+#b=330
+#print("A")if a>b else print("B")
+
+#a=1000
+#b=330
+#print("A")if a>b else print("=")if a==b else print("B")
+
+
+#num1=int(input("insert the num1"))
+#num2=int(input("insert the num2"))
+
+#if num1>num2:
+#    print(num1,num2)
+#elif num1<num2:
+ #   print(num2,num1)
+    ########
+#name=input("insert your name")
+#age=int(input("insert your age"))  
+
+#if age<18:
+ #     print("under age ")
+#avg=int(input("insert ypur avg"))
+#if avg>=90:
+   #       print("excelent avg")
+#elif  avg>=50 and avg<=90:
+ #     print("passed")
+#else:
+    #  print("failed ")
+      
+#if age>18 :
+         # print("Adult")
+         # job=input("insert the job")
+         # print(age,name,job)
+         
+         
+         
+#for a in range(11):
+   #   print("*") 
+
+ 
+#i=0
+#while i<20:
+  #   print("*",end='')
+  #   i+=1
+    
+  
+#for i in range(10):
+   #  for b in range(i):
+    #    print("*",end="")
+    # print()
+    
+#for i in range (10):
+   #  for j in range (i):
+     #  print("*",end="")
+    # print()
+    
+#while True:
+# try: 
+#    n=int(input("insert num"))
+#    break
+# except ValueError:
+#     print("no valid num")
+#print("valid num")    
+         
+   
+            
+     
+     
+     
+    
+
+        
